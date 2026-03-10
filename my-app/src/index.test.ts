59bc005c709b46568d1ca321b0a4ea6871fe5b1c
kazurayam <kazuaki.urayama@gmail.com>
2026-03-10 20:35:31 +0900

    implement the index.ts and its test

---
+++ my-app/src/index.test.ts
@@ -0,0 +1,35 @@
+// src/index.test.ts
+import { describe, it, expect } from 'bun:test';
+import { testClient } from 'hono/testing'
+import app from './index';
+
+describe('getting started', () => {
+    it('GET /', async () => {
+        const res = await app.request('/', { method: 'GET' });
+        expect(res.status).toBe(200);
+    });
+});
+
+describe('verify HTML as Text', async () => {
+    const client: any = testClient(app)
+    it("should return HTML content with 'Hello Hono!'", async () => {
+        const res = await client.$get({})
+        expect(res.status).toBe(200)
+        const text = await res.text()
+        console.log(text)
+        expect(text).toContain('Hello Hono!')
+    })
+})
+
+describe('verify HTML as DOM', async () => {
+    const client: any = testClient(app)
+    it("should return HTML content with 'Hello Hono!'", async () => {
+        const res = await client.$get({})
+        expect(res.status).toBe(200)
+        const text = await res.text()
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(text, 'text/html')
+        const pre = doc.querySelector('p')
+        expect(pre?.textContent).toContain('Hello Hono!')
+    })
+})
--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from 'hono'
+import { html } from 'hono/html'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+    return c.html(
+        html`<!doctype html>
+        <html>
+            <head>
+                <title>Welcome nobody</title>
+            </head>
+            <body>
+                <p>Hello Hono!</p>
+            </body>
+        </html>
+        `
+    )
 })
 
 export default app
